a7dd5386091dcd2542afe196f32b7e5d12f3e967
Fouad Abdelhady <fouad.abd-elhady@outlook.com>
2023-08-01 21:48:20 +0300

    added: authorization for category update,delete,add

--- api/routes/categoriesRoutes.js
+++ api/routes/categoriesRoutes.js
@@ -3,9 +3,11 @@
 const express = require('express');
 const router = express.Router();
 const categoriesController = require('../controllers/categoriesController');
+const authorization = require('../utils/authorization');
+const constants = require('../utils/constants');
 
 // Create a new category
-router.post('/', categoriesController.createCategory);
+router.post('/', authorization.verifyAccess(constants.Actions.addCategory), categoriesController.createCategory);
 
 // Get all categories
 router.get('/', categoriesController.getAllCategories);
@@ -14,9 +16,9 @@ router.get('/', categoriesController.getAllCategories);
 router.get('/:categoryId', categoriesController.getCategoryById);
 
 // Update a category by ID
-router.put('/:categoryId', categoriesController.updateCategoryById);
+router.put('/:categoryId', authorization.verifyAccess(constants.Actions.updateCategory), categoriesController.updateCategoryById);
 
 // Delete a category by ID
-router.delete('/:categoryId', categoriesController.deleteCategoryById);
+router.delete('/:categoryId', authorization.verifyAccess(constants.Actions.deleteCategory), categoriesController.deleteCategoryById);
 
 module.exports = router;
