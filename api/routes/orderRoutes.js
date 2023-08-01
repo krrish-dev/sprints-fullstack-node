c3277e3768617762e47dbaef2c561d34f12d61d9
Fouad Abdelhady <fouad.abd-elhady@outlook.com>
2023-08-01 21:54:37 +0300

    minor changes

--- api/routes/orderRoutes.js
+++ api/routes/orderRoutes.js
@@ -5,14 +5,7 @@ const authorization = require('../utils/authorization');
 const constants = require('../utils/constants');
 
 router.post('/order', authorization.verifyAccess(constants.Actions.placeOrder), orderController.checkOut);
-router.get('/order', (req, res)=>{
-    res.send("Here is the order");
-});
-router.post('/newOrder', (req, res)=>{
-    res.send("Here is the new order");
-});
 router.post('/paymentSuccess', authorization.verifyAccess(constants.Actions.placeOrder), orderController.onPaymentSuccess);
 router.get('/paymentCanceled', authorization.verifyAccess(constants.Actions.placeOrder), orderController.onPaymentCancelled);
 router.post('/webhook', express.raw({type: 'application/json'}),orderController.onPaymentStatusChange);
 module.exports = router;
-//'cs_test_b1kA1JU3PMFzcfsmvkZeB20Xht60FB0jk1XnXKL9AJ8ggRDUWJMkmuvmI5'
