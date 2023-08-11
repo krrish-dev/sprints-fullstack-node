416d9649f0cd686c3bd59e5f148ee632b75922c0
LoayEssawy <loay.elessawy@gmail.com>
2023-08-11 15:58:19 +0300

    update Checkout page 1

--- Frontend/js/checkout.js
+++ Frontend/js/checkout.js
@@ -46,30 +46,3 @@ menuInputs.forEach((input, index) => {
 
 
 
-
-// Wasted 2 hours on that, just ignore
-
-
-// const checkIfOtherCheckboxIsSelected = (currentSelected) => {
-//   // console.log(Array.from(menuInputs).some((input, index) => input.checked && index != currentSelected))
-//   // // return Array.from(menuInputs).some((input, index) => input.checked && index != currentSelected)
-//   // console.log(Array.from(menuInputs).every(input => input.checked))
-//   // return Array.from(menuInputs).every(input => input.checked)
-//   // const otherRadioButtons = Array.from(menuInputs).filter((_, index) => index != currentSelected)
-//   // console.log(otherRadioButtons.some(button => button.checked), otherRadioButtons, otherRadioButtons[2].checked)
-//   // return otherRadioButtons.some(button => button.checked)
-
-//   // let isOtherButtonClicked = false
-//   // menuInputs.forEach((button, index) => {
-//   //   if (index === currentSelected) return
-//   //   console.log(button.checked)
-
-//   //   if (button.checked) {
-//   //     console.log("sd")
-//   //     isOtherButtonClicked = true
-//   //   }
-//   // })
-
-//   // return isOtherButtonClicked
-// }
-
